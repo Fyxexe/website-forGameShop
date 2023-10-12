4e20039b97a19a83ba6ba7e7949efaa56266257f
n1ckero <artyomhovhannisyan009@gmail.com>
2023-10-12 09:21:53 +0400

    header code complited

--- src/components/header.php
+++ src/components/header.php
@@ -17,12 +17,16 @@
    </ul>
 
    <div class="header-info-box">
-    <div class="info_box_tex">
+    <div class="info_box_text">
         <h1>Crypto game shop</h1>
         <p>the largest and coolest gaming store in the entire CIS, 
             the best players buy from us, and those who don’t buy should buy</p>
+            <div class="info_buttons">
+            <button class="button-79" role="button">Go to Products</button>
+            <button class="button-78" role="button">more info</button>
+            </div>
     </div>
-      <img src="/Web2/public/icons/game.png" alt="Game Image">
+      <img src="/Web2/public/icons/game.png" alt="Game Image" id='header_info_image'>
    </div>
 </div>
 
