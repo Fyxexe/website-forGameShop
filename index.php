74bb845ef55247616714e480057b50c3893ca586
n1ckero <artyomhovhannisyan009@gmail.com>
2023-10-12 08:57:46 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/assets/style/main.css">
+    <link rel="stylesheet" href="./src/assets/style/media.css">
+    <title>Home</title>
+</head>
+<body>
+    <header class="header">
+    <?php
+        include './src/components/header.php';
+    ?>
+    </header>
+
+
+    <footer>
+    <?php
+        include './src/components/footer.php';
+    ?>
+    </footer>
+    
+</body>
+</html>
---
+++ src/components/header.php
@@ -0,0 +1,29 @@
+<?php 
+
+?>
+
+<div class="container">
+   <ul class="menu">
+
+    <li class="menu-logo"><h1>Crypto</h1></li>
+    <li class="menu-bar">
+        <ul class="nav-bar">
+            <li class="nav-item">Home</li>
+            <li class="nav-item">Products</li>
+            <li class="nav-item">About Us</li>
+            <li class="nav-item">Feedback</li>
+        </ul>
+    </li>
+   </ul>
+
+   <div class="header-info-box">
+    <div class="info_box_tex">
+        <h1>Crypto game shop</h1>
+        <p>the largest and coolest gaming store in the entire CIS, 
+            the best players buy from us, and those who don’t buy should buy</p>
+    </div>
+      <img src="/Web2/public/icons/game.png" alt="Game Image">
+   </div>
+</div>
+
+</html>
