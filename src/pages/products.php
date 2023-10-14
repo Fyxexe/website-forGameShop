bf96cdac369dc5e0ba1e1b6a3459c6fedd5fb215
n1ckero <artyomhovhannisyan009@gmail.com>
2023-10-14 13:00:03 +0400

    products fix

--- src/pages/products.php
+++ src/pages/products.php
@@ -10,13 +10,28 @@ $product1 = [
 ];
 
 $product2 = [
-    'title' => 'GTA 5',
+    'title' => 'CS:GO',
     'price' => 8.40 ,
-    'imgPath' => '../../public/products/productOne_gta.jpeg',
-    'moreInfo' => "Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the seventh main entry in the Grand Theft Auto series, following 2008's Grand Theft Auto IV, and the fifteenth instalment overall. Set within the fictional state of San Andreas, based on Southern California, the single-player story follows three protagonists—retired bank robber Michael De Santa, street gangster Franklin Clinton, and drug dealer and gunrunner Trevor Philips—and their attempts to commit heists while under pressure from a corrupt government agency and powerful criminals. The open world design lets players freely roam San Andreas's open countryside and the fictional city of Los Santos, based on Los Angeles.",
+    'imgPath' => '../../public/products/pr2.jpg',
+    'moreInfo' => "counter-Strike: Global Offensive (CS:GO) is a 2012 multiplayer first-person shooter developed by Valve and Hidden Path Entertainment. It is the fourth game in the Counter-Strike series. Developed for over two years, Global Offensive was released for Windows, macOS, Xbox 360, and PlayStation 3 in August 2012, and for Linux in 2014. Valve still regularly updates the game, both with smaller balancing patches and larger content additions",
     'index' => 2
 ];
- 
+$product3 = [
+    'title' => 'Assassins Creed',
+    'price' => 8.40 ,
+    'imgPath' => '../../public/products/pr3.jpg',
+    'moreInfo' => "Assassin's Creed is an open-world action-adventure stealth video game franchise published by Ubisoft and developed mainly by its studio Ubisoft Montreal using the game engine Anvil and its more advanced derivatives. Created by Patrice Désilets, Jade Raymond, and Corey May, the Assassin's Creed series depicts a fictional millennia-old struggle between the Assassins, who fight for peace and free will, and the Templars, who desire peace through order and control",
+    'index' => 3
+];
+$product4 = [
+    'title' => 'Assassins Creed',
+    'price' => 8.40 ,
+    'imgPath' => '../../public/products/pr3.jpg',
+    'moreInfo' => "Assassin's Creed is an open-world action-adventure stealth video game franchise published by Ubisoft and developed mainly by its studio Ubisoft Montreal using the game engine Anvil and its more advanced derivatives. Created by Patrice Désilets, Jade Raymond, and Corey May, the Assassin's Creed series depicts a fictional millennia-old struggle between the Assassins, who fight for peace and free will, and the Templars, who desire peace through order and control",
+    'index' => 4
+];
+
+$products = [$product1,$product2,$product3,$product4];
 ?>
 
 <!DOCTYPE html>
@@ -24,8 +39,8 @@ $product2 = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".././assets/style/main.css">
-    <link rel="stylesheet" href=".././assets/style/media.css">
+    <link rel="stylesheet" href="../assets/style/main.css">
+    <!-- <link rel="stylesheet" href=".././assets/style/media.css"> -->
     <title>Home</title>
 </head>
 <body>
@@ -40,46 +55,17 @@ $product2 = [
     <main>
         <div class="container">
         <div class="products-box">
-            <div class="products-row">
+            <?php foreach ($products as $value) { ?>
                 <div class="product_box">
-                    <img src="<?php echo $product1['imgPath'];?>" alt="">
-                    <h1><?php echo $product1['title']; ?></h1>
-                    <p> <?php echo $product1['price'] . '$'; ?></p>
-                    <button class="button-18" role="button" id="<?php echo $product1['index'];?>"><a href="#!">Buy!</a></button>
-                </div>
-                <div class="product_box">
-                    <img src="<?php echo $product2['imgPath'];?>" alt="">
-                    <h1><?php echo $product2['title']; ?></h1>
-                    <p> <?php echo $product2['price'] . '$'; ?></p>
-                    <button class="button-18" role="button" id="<?php echo $product2['index'];?>"><a href="#!">Buy!</a></button>
-                </div>
-                <div class="product_box">
-                    <img src="<?php echo $product2['imgPath'];?>" alt="">
-                    <h1><?php echo $product2['title']; ?></h1>
-                    <p> <?php echo $product2['price'] . '$'; ?></p>
-                    <button class="button-18" role="button" id="<?php echo $product2['index'];?>"><a href="#!">Buy!</a></button>
-                </div>
+                    <img src="<?php echo $value['imgPath'] ?>" alt="">
+                    <h1><?php echo $value['title'] ;?></h1>
+                    <p><?php echo $value['price']?></p>
+                    <button class="button-18"> more info</button>
+                
             </div>
-            <div class="products-row">
-                <div class="product_box">
-                    <img src="<?php echo $product1['imgPath'];?>" alt="">
-                    <h1><?php echo $product1['title']; ?></h1>
-                    <p> <?php echo $product1['price'] . '$'; ?></p>
-                    <button class="button-18" role="button" id="<?php echo $product1['index'];?>"><a href="#!">Buy!</a></button>
-                </div>
-                <div class="product_box">
-                    <img src="<?php echo $product2['imgPath'];?>" alt="">
-                    <h1><?php echo $product2['title']; ?></h1>
-                    <p> <?php echo $product2['price'] . '$'; ?></p>
-                    <button class="button-18" role="button" id="<?php echo $product2['index'];?>"><a href="#!">Buy!</a></button>
-                </div>
-                <div class="product_box">
-                    <img src="<?php echo $product2['imgPath'];?>" alt="">
-                    <h1><?php echo $product2['title']; ?></h1>
-                    <p> <?php echo $product2['price'] . '$'; ?></p>
-                    <button class="button-18" role="button" id="<?php echo $product2['index'];?>"><a href="#!">Buy!</a></button>
-                </div>
-            </div>
+            
+            <?php } ?>
+       
         </div>
         </div>
     </main>
